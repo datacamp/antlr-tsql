583ecd6607cc664e8ba8ecfa1a207a927c281c65
Michael Chow <machow@princeton.edu>
2017-04-17 11:36:58 -0400

    modify unit tests to dump ast reprs, so can test between changes

--- antlr_tsql/tests/test_ast.py
+++ antlr_tsql/tests/test_ast.py
@@ -14,13 +14,36 @@ def test_ast_parse_strict():
 
 @pytest.mark.parametrize('fname', [
         'visual_checks.yml',
-        'v0.3.yml'
+        'v0.3.yml',
+        #'v0.4.yml'
         ])
 def test_ast_examples_parse(fname):
     # just make sure they don't throw error for now..
     import yaml
     dirname = os.path.dirname(__file__)
     data = yaml.load(open(dirname + '/' + fname))
+    res = {}
     for start, cmds in data['code'].items():
-        [ast.parse(cmd, start, strict=True) for cmd in cmds]
+        res[start] = []
+        for cmd in cmds: res[start].append([cmd, repr(ast.parse(cmd, start, strict=True))])
+    print(res)
+    with open(dirname + '/dump_' + fname, 'w') as out_f:
+        yaml.dump(res, out_f)
 
+def load_dump(fname):
+    import yaml
+    dirname = os.path.dirname(__file__)
+    dump_data = yaml.load(open(dirname + '/' + fname))
+
+    all_cmds = []
+    for start, cmds in dump_data.items():
+        for cmd, res in cmds: all_cmds.append((start, cmd, res))
+    return all_cmds
+
+@pytest.mark.parametrize('start,cmd,res', [
+        *load_dump('dump_visual_checks.yml'),
+        *load_dump('dump_v0.3.yml'),
+        #'v0.4.yml'
+        ])
+def test_dump(start, cmd, res):
+    assert repr(ast.parse(cmd, start, strict=True)) == res
